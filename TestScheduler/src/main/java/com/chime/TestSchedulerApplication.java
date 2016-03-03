0ef8dd90182c009be7fe650b550e3716e2a19144
chkwak <chkwak@chkwak-PC>
2016-03-03 16:47:36 +0900

    log 설정

--- TestScheduler/src/main/java/com/chime/TestSchedulerApplication.java
+++ TestScheduler/src/main/java/com/chime/TestSchedulerApplication.java
@@ -1,5 +1,7 @@
 package com.chime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,10 +9,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class TestSchedulerApplication {
+	
+	private static  final Logger LOGGER = LoggerFactory.getLogger(TestSchedulerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSchedulerApplication.class, args);
 		
-		System.out.println("# Daemons Start ... ");
+		LOGGER.info("# Daemons Start ... "); 
+		
 	}
 }
